7f8849e7582a1e406c41ee86f823e744e964e83b
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-07-12 21:42:38 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  alpha_hash = {}
+
+  alphabet.each_with_index do |letter, idx|
+    alpha_hash[letter] = idx
+  end
+
+  new_arr = arr.sort_by do |item|
+    item.chars.map { |letter| alpha_hash[letter]}
+  end
+
+  new_arr
 end
